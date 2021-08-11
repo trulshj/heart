0bf96009e0e17eb4cfe4c54f1d36cdc9aecc9bf3
Truls Henrik Jakobsen <trulshj@stud.ntnu.no>
2021-08-11 03:21:24 +0200

    Add Credit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 function App() {
-    return (
-        <div className='App'>
-            <Heart />
-            <footer>Made with <strong>love</strong> by <a href="www.github.com/trulshj">truls</a></footer>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Heart />
+      <div class="credit">
+        made with love by <a href="https://www.github.com/trulshj" target='_blank' rel='noreferrer'>truls</a>
+      </div>
+    </div>
+  );
 }
 
 function Heart() {
-    return (
-        <div>
-            <div class='back'></div>
-            <div class='heart'></div>
-        </div>
-    );
+  return (
+    <div>
+      <div class="back"></div>
+      <div class="heart"></div>
+    </div>
+  );
 }
 
 export default App;
